632cd93f1e549ba9e1ac22971cff9132a9996d17
julien <julienalaria@gmail.com>
2025-09-20 21:46:56 +0200

    exos 11 & 12

---
+++ exo11.php
@@ -0,0 +1,48 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+abstract Class Forme {
+
+    abstract public function calculerAire(): float;
+}
+
+class Cercle extends Forme {
+    public float $rayon;
+
+    public function __construct(float $rayon) {
+        $this->rayon = $rayon;
+    }
+
+    public function calculerAire(): float {
+        return pi() * pow($this->rayon, 2);
+    }
+
+    public function getRayon(): float {
+        return $this->rayon;
+    }
+}
+
+class Rectangle extends Forme {
+    public float $longueur;
+    public float $largeur;
+
+    public function __construct(float $longueur, float $largeur) {
+        $this->longueur = $longueur;
+        $this->largeur = $largeur;
+    }
+
+    public function calculerAire(): float {
+        return $this->longueur * $this->largeur;
+    }
+
+    public function getDimensions(): string {
+        return "Longueur : $this->longueur, Largeur : $this->largeur";
+    }
+}
+
+$cercle = new cercle(5);
+$rectangle = new Rectangle(4, 6);
+
+echo "Aire du cercle de rayon {$cercle->getRayon()} : " . $cercle->calculerAire() . " cm²<br>";
+echo "Aire du rectangle ({$rectangle->getDimensions()}) : " . $rectangle->calculerAire() . " cm²<br>";
---
+++ exo12.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+interface Volume {
+    public function getVolume(): float;
+}
+
+class Cube implements Volume {
+    private float $cote;
+
+    private function __construct(float $cote) {
+        $this->cote = $cote;
+    }
+
+    public function getVolume(): float {
+        return pow($this->cote, 3);
+    }
+
+    public function getCote(): float {
+        return $this->cote;
+    }
+}
+
+class Cylindre implements Volume {
+    private float $rayon;
+    private float $hauteur;
+
+    public function __construct(float $rayon, float $hauteur) {
+        $this->rayon = $rayon;
+        $this->hauteur = $hauteur;
+    }
+
+    public function getVolume(): float {
+        return pi() * pow($this->rayopn, 2) * $this->hauteur;
+    }
+
+    public function getDimensions(): string {
+        return "Rayon : {$this->rayon}, Heuteur : {$this->hauteur}";
+    }
+}
+
+$cube1 = new Cube(3);
+$cylindre1 = new Cylindre(2, 5);
+
+echo "Volume du cube de côté {$cube->getCote()} : " . $cube->getVolume() . " cm³<br>";
+echo "Volume du cylindre ({$cylindre->getDimensions()}) : " . $cylindre->getVolume() . " cm³";
